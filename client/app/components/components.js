9b8764f9c7ef25a1d45af7b3186ad151fddfe100
Popeye <liew.seaw.wei@gmail.com>
2018-01-24 18:37:13 +0800

    Remove unused components and update readme

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,12 +1,10 @@
 import angular from 'angular';
 import Home from './home/home';
-import About from './about/about';
 import Q1 from './q1/q1';
 import Q2 from './q2/q2';
 
 let componentModule = angular.module('app.components', [
   Home,
-  About,
   Q1,
   Q2,
 ])
